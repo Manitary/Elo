fc6a9aec3b190f22ce34b85325745ef5a0d2b8fc
Manitary <pak.ska@gmail.com>
2023-08-15 11:13:30 +0100

    Partially update elo_other.py
    Impossible to finish without knowing the intention
    Unclear code in multiple parts, could be duplicated or a mistake

--- elosports/elo_other.py
+++ elosports/elo_other.py
@@ -1,11 +1,11 @@
-from operator import itemgetter
 from collections import defaultdict
-import scipy.stats as st
-import numpy as np
+from operator import itemgetter
+
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 
 
-def k_factor(margin_of_victory, elo_diff):
+def k_factor(margin_of_victory: float, elo_diff: float) -> tuple[float, float]:
     init_k = 20
     if margin_of_victory > 0:
         multiplier = (margin_of_victory + 3) ** (0.8) / (7.5 + 0.006 * (elo_diff))
@@ -14,42 +14,45 @@ def k_factor(margin_of_victory, elo_diff):
     return init_k * multiplier, init_k * multiplier
 
 
-def s_value(home_score, away_score):
-    S_home, S_away = 0, 0
+def s_value(home_score: float, away_score: float) -> tuple[float, float]:
     if home_score > away_score:
-        S_home = 1
-    elif away_score > home_score:
-        S_away = 1
-    else:
-        S_home, S_away = 0.5, 0.5
-    return S_home, S_away
+        return 1, 0
+    if home_score < away_score:
+        return 0, 1
+    return 0.5, 0.5
 
 
-def elo_update(home_score, away_score, home_rating, away_rating, home_advantage=100.0):
+def elo_update(
+    home_score: float,
+    away_score: float,
+    home_rating: float,
+    away_rating: float,
+    home_advantage: float = 100.0,
+) -> tuple[float, float]:
     home_rating += home_advantage
     elo_home = elo_prediction(home_rating, away_rating)
     elo_away = 1 - elo_home
     elo_diff = home_rating - away_rating
-    MOV = home_score - away_score
+    mov = home_score - away_score
 
     s_home, s_away = s_value(home_score, away_score)
     if s_home > 0:
-        K_home, K_away = k_factor(MOV, elo_diff)
+        k_home, k_away = k_factor(mov, elo_diff)
     else:
-        K_home, K_away = k_factor(MOV, elo_diff)
+        k_home, k_away = k_factor(mov, elo_diff)
 
-    return K_home * (s_home - elo_home), K_away * (s_away - elo_away)
+    return k_home * (s_home - elo_home), k_away * (s_away - elo_away)
 
 
-def elo_prediction(home_rating, away_rating):
+def elo_prediction(home_rating: float, away_rating: float) -> float:
     return 1.0 / (1 + 10 ** ((away_rating - home_rating) / (400.0)))
 
 
-def score_prediction(home_rating, away_rating):
+def score_prediction(home_rating: float, away_rating: float) -> float:
     return (home_rating - away_rating) / 28.0
 
 
-class EloSimulation(object):
+class EloSimulation:
     def __init__(
         self,
         games,
@@ -57,7 +60,7 @@ class EloSimulation(object):
         label_dict,
         end_of_season_correction,
         prediction_function=None,
-    ):
+    ) -> None:
         self.update_function = update_function
         self.games = games
         self.ratings = {}
@@ -67,7 +70,7 @@ class EloSimulation(object):
         self.end_of_season_correction = end_of_season_correction
 
     def train(self):
-        for idx, game in self.games:
+        for game in self.games:
             new_year = game[label_dict["year_id"]]
             label_i = game[label_dict["fran_id"]]
             label_j = game[label_dict["opp_fran"]]
@@ -102,7 +105,6 @@ class EloSimulation(object):
             self.ratings[label_j] += update[1]
 
     def power_rankings(self):
-
         power_rankings = sorted(self.ratings.items(), key=itemgetter(1), reverse=True)
         power = []
         for i, x in enumerate(power_rankings):
@@ -114,7 +116,7 @@ label_dict = {
     "year_id": "year_id",
     "fran_id": "fran_id",
     "opp_fran": "opp_fran",
-    "gameorder": "gameorder",
+    "game_order": "game_order",
     "pts": "pts",
     "opp_pts": "opp_pts",
 }
@@ -129,12 +131,12 @@ STARTING_LOC = 0
 
 def elo_lookup(fran_id, gameorder):
     return full_df[
-        (full_df["fran_id"] == fran_id) & (full_df["gameorder"] >= gameorder)
+        (full_df["fran_id"] == fran_id) & (full_df["game_order"] >= gameorder)
     ]["elo_i"].iloc[0]
 
 
 m = EloSimulation(
-    games=list(games[games["gameorder"] > STARTING_LOC].iterrows()),
+    games=list(games[games["game_order"] > STARTING_LOC].iterrows()),
     update_function=elo_update,
     prediction_function=elo_prediction,
     label_dict=label_dict,
@@ -147,7 +149,6 @@ games["prediction"] = m.predictions
 games["predictedWinner"] = games["prediction"].apply(lambda x: 1 if x >= 0.5 else 0)
 games["winner"] = games.apply(lambda x: x["pts"] >= x["opp_pts"], axis=1)
 
-from sklearn.metrics import confusion_matrix
 
 conf_matrix = confusion_matrix(games["winner"], games["predictedWinner"])
 
